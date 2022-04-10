f9c8527f48b0f75ecba6a77a236e45c5b076ebf7
Felipe Mitjans <fmitjans@uc.cl>
2022-04-10 19:22:30 -0400

    scan function finished

--- T2/src/queue/queue.h
+++ T2/src/queue/queue.h
@@ -12,7 +12,7 @@ void enqueue(Process* process, Queue* queue);
 // remove process from queue and return pointer
 Process* dequeue(Queue* queue);
 // pendiente: scan for old processes
-void scan(Queue* queue1, Queue* queue2, Queue* queue3);
+void scan(Queue* queue1, Queue* queue2, Queue* queue3, int current_t);
 
 void destroy_queue(Queue* queue);
 void print_queue(Queue* queue, char* name);
